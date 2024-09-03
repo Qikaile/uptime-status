28c17119cbb74bb5c34b1a41d1a2f6646af4c3e0
浅影〃 <2649019047@qq.com>
2024-09-03 12:04:12 +0800

    Update uptimerobot.js

--- src/common/uptimerobot.js
+++ src/common/uptimerobot.js
@@ -27,7 +27,7 @@ export async function GetMonitors(apikey, days) {
     custom_uptime_ranges: ranges.join('-'),
   };
 
-  const response = await axios.post('https://uptimestatus.qikaile.tk/v2/getMonitors', postdata, {
+  const response = await axios.post('https://api.uptimerobot.com/v2/getMonitors', postdata, {
     timeout: 10000
   });
   if (response.data.stat !== 'ok') {
